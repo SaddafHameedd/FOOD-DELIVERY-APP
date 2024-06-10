9d6f436f75df5df80f87a5e21f92b980dc770d4d
saddafdev <sadafhameed46@gmail.com>
2024-06-10 23:48:22 +0500

    y

---
+++ Food-Delivery-App/src/Components/Carousel.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import burger from "../assets/burger.jpg";
+import pastry from "../assets/pastry.jpg";
+import barbeque from "../assets/barbeque.jpg";
+
+function Carousel() {
+  return (
+    <div>
+      <div
+        id="carouselExampleFade"
+        className="carousel slide carousel-fade"
+        data-bs-ride="carousel"
+        style={{ objectFit: "contain !important" }}
+      >
+        <div className="carousel-inner" id="carousel" >
+          <div className="carousel-caption" style={{ zIndex: "10" }}>
+            <form className="d-flex">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button
+                className="btn btn-outline-success text-white bg-success"
+                type="submit"
+              >
+                Search
+              </button>
+            </form>
+          </div>
+          <div className="carousel-item active">
+            <img
+              src={burger}
+              className="d-block w-100"
+              alt="Burger"
+              style={{ filter: "brightness(40%)" }}
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src={pastry}
+              className="d-block w-100"
+              alt="Pastry"
+              style={{ filter: "brightness(30%)" }}
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src={barbeque}
+              className="d-block w-100"
+              alt="Barbeque"
+              style={{ filter: "brightness(30%)" }}
+            />
+          </div>
+        </div>
+        <a
+          className="carousel-control-prev"
+          href="#carouselExampleFade"
+          role="button"
+          data-bs-slide="prev"
+        >
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="sr-only">Previous</span>
+        </a>
+        <a
+          className="carousel-control-next"
+          href="#carouselExampleFade"
+          role="button"
+          data-bs-slide="next"
+        >
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="sr-only">Next</span>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Carousel;
--- Food-Delivery-App/src/screens/Home.jsx
+++ Food-Delivery-App/src/screens/Home.jsx
@@ -1,14 +1,20 @@
-import Footer from "../Components/Footer"
-import Navbar from "../Components/Navbar"
-
+import Card from "../Components/Card";
+import Carousel from "../Components/Carousel";
+import Footer from "../Components/Footer";
+import Navbar from "../Components/Navbar";
 
 function App() {
   return (
     <div>
-      <Navbar/>
-      <Footer/>
+      <Navbar />
+      <div><Carousel/></div>
+      <div className="m-3"><Card/></div>
+      <div className="m-3"><Card/></div>
+      <div className="m-3"><Card/></div>
+      <div className="m-3"><Card/></div>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
