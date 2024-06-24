cbb2db99a5cef94959875adcd87b1a45fe5c3ff5
saddafdev <sadafhameed46@gmail.com>
2024-06-24 20:35:11 +0500

    new commit

--- Food-Delivery-App/Backend/index.js
+++ Food-Delivery-App/Backend/index.js
@@ -6,11 +6,8 @@ const port = 5000;
 app.get('/', (req, res) => {
   res.send('Hello world');
 });
-
 app.use(express.json());
-
 app.use('/api', require('./Routes/CreateUser'));
-
 app.listen(port, () => {
   console.log('Example app listening on port ' + port);
 });
