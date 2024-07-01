6af7e3868ef248e5a8ac5acb2282c09ac0b3940b
saddafdev <sadafhameed46@gmail.com>
2024-07-01 23:09:37 +0500

    ..

--- Food-Delivery-App/src/screens/Home.jsx
+++ Food-Delivery-App/src/screens/Home.jsx
@@ -4,10 +4,6 @@ import Footer from "../Components/Footer";
 import Navbar from "../Components/Navbar";
 function App() {
   return (
-
-
-
-    
     <div>
       <Navbar />
       <div><Carousel/></div>
