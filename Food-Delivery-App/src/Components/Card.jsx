8be978e4f81a29c7962c7106b496de85e8716e28
saddafdev <sadafhameed46@gmail.com>
2024-07-02 20:45:54 +0500

    new changes added

--- Food-Delivery-App/src/Components/Card.jsx
+++ Food-Delivery-App/src/Components/Card.jsx
@@ -4,6 +4,10 @@ function Card() {
       <div className="card mt-3" style={{ width: "18rem", maxHeight: "360px" }}>
         <img className="card-img-top" src="https://as2.ftcdn.net/v2/jpg/03/99/84/49/1000_F_399844901_hqsm8dnV9kRxBjEyeDy5xdvtCUEXgdIE.jpg" alt="Card image cap" />
         <div className="card-body">
+
+
+
+          
           <h5 className="card-title">Card title</h5>
           <p className="card-text">Some quick the cards content</p>
           <div className="container w-100">
