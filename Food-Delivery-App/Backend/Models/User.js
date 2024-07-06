ba36aa8e0afed487070fa53efef9c281fd56bf40
saddafdev <sadafhameed46@gmail.com>
2024-07-06 16:12:38 +0500

    changes

--- Food-Delivery-App/Backend/Models/User.js
+++ Food-Delivery-App/Backend/Models/User.js
@@ -7,10 +7,6 @@ const UserSchema = new Schema(
     email:{type:String, required:true},
     password:{type:String, required:true},
     date:{type:Date, default:Date.now},
-
-
-
-    
   }
 )
 module.exports = mongoose.model('user',UserSchema)
