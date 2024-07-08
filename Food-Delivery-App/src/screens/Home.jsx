97807cd8d791b01da66b5620b3a3ec2cbecc27b7
saddafdev <sadafhameed46@gmail.com>
2024-07-08 23:50:53 +0500

    changes

--- Food-Delivery-App/src/screens/Home.jsx
+++ Food-Delivery-App/src/screens/Home.jsx
@@ -3,6 +3,13 @@ import Carousel from "../Components/Carousel";
 import Footer from "../Components/Footer";
 import Navbar from "../Components/Navbar";
 function App() {
+
+
+
+
+
+
+
   return (
     <div>
       <Navbar />
