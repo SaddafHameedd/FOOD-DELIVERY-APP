36a0d8c4c5ce964c9a4b5fd11710ac9a8f8ef147
saddafdev <sadafhameed46@gmail.com>
2024-07-12 23:40:40 +0500

    commit

--- Food-Delivery-App/src/Components/Footer.jsx
+++ Food-Delivery-App/src/Components/Footer.jsx
@@ -1,5 +1,14 @@
 import {Link} from 'react-router-dom'
 function Footer() {
+
+
+
+
+
+
+
+
+
   return (
     <div>
       <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
