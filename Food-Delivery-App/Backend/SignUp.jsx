e02f3fd76c8e369db1d357db7e4d1ea2f7e0b262
saddafdev <sadafhameed46@gmail.com>
2024-07-15 23:23:28 +0500

    commit

--- Food-Delivery-App/Backend/SignUp.jsx
+++ Food-Delivery-App/Backend/SignUp.jsx
@@ -5,10 +5,6 @@ function SignUp() {
     e.preventDefault();
     const respose = fetch('https://localhost:5000/api/createuser'),{
 
-
-
-
-      
     method:'POST',
     header:{
       'Content-Type':'application/json'
