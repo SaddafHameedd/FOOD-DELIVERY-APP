a45388b1ec3c20729dcb0bfe4efc1a47fbae30b1
saddafdev <sadafhameed46@gmail.com>
2024-07-17 23:43:21 +0500

    changes

--- Food-Delivery-App/Backend/Models/User.js
+++ Food-Delivery-App/Backend/Models/User.js
@@ -1,6 +1,10 @@
 const mongoose= require('mongoose')
 const {Schema} =mongoose;
 const UserSchema = new Schema(
+
+
+
+  
   {
     name:{type:String, required:true},
     location:{type:String, required:true},
